df5637103845b003d5f7104ccf529aad35f55c2b
brayansu44 <brayansayrez400@gmail.com>
2025-03-23 19:52:34 -0500

    Nuevas modificaciones en el modulo de bodega

--- notificaciones/admin.py
+++ notificaciones/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Notificacion
 
-# Register your models here.
+@admin.register(Notificacion)
+class NotificacionAdmin(admin.ModelAdmin):
+    list_display = ("user", "mensaje_resumido", "tipo", "leida", "fecha_creacion")
+    list_filter = ("tipo", "leida", "fecha_creacion")
+    search_fields = ("user__username", "mensaje")
+    list_editable = ("leida",)
+    readonly_fields = ("fecha_creacion",)
+
+    def mensaje_resumido(self, obj):
+        return obj.mensaje[:50] + "..." if len(obj.mensaje) > 50 else obj.mensaje
+
+    mensaje_resumido.short_description = "Mensaje"
+
+    def has_add_permission(self, request):
+        """Evita que los usuarios agreguen notificaciones manualmente desde el admin."""
+        return False
