7b87965bf77a106ae65a6bc5f37e942838d92c62
Kevin Marquez <jakej2747@gmail.com>
2025-04-13 14:33:36 -0500

    Modulo de Nomina se genera template nomina y seguridadsocial

--- nomina/views.py
+++ nomina/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from .models import Nomina, EPS, ARL, Pension, CajaCompensacion
+
+@login_required(login_url = 'login')
+def nomina(request):
+    nomina        = Nomina.objects.all()
+
+    return render(request, 'nomina/nomina.html', {'nomina': nomina})
+
+@login_required(login_url = 'login')
+def SeguridadSocial(request, tab_id):
+    eps     = EPS.objects.all()
+    arl     = ARL.objects.all()
+    pension = Pension.objects.all()
+    cajacompensacion = CajaCompensacion.objects.all()
+
+    context = {
+        'tab_id': tab_id,
+        'eps': eps,
+        'arl': arl,
+        'pension': pension,
+        'cajacompensacion': cajacompensacion,
+    }
+
+    return render(request, 'nomina/SeguridadSocial.html', context)
