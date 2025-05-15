95dcd61bd5c48cd57b72704fdf3ff2fb4318b0ae
Brayan Alexander Suarez Ropero <bsuarez@ins.gov.co>
2025-05-15 16:20:47 -0500

    CRUD en el modulo de productos

---
+++ Producto/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import *
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = [
+            'codigo', 'referencia', 'diseno',
+            'color', 'talla', 'categoria',
+            'genero', 'estado', 'descripcion'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
+        self.fields['categoria'].empty_label = "Seleccione una categoria"
+        self.fields['categoria'].widget.attrs.update({'class': 'form-select'})
+
+        self.fields['genero'].empty_label = "Seleccione un genero"
+        self.fields['genero'].widget.attrs.update({'class': 'form-select'})
+
+        self.fields['estado'].widget.attrs.update({'class': 'form-select'})
+
+        if 'descripcion' in self.fields:
+            descripcion_field = self.fields.pop('descripcion')  
+            self.fields['descripcion'] = descripcion_field
